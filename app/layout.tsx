f5c0397867bc9047e8cbcfe20a8aa47a2a86fd22
manlikexavy <chibokaxavier@gmail.com>
2024-07-10 17:22:47 +0100

    created pages,a nd set up global css

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+import Header from ".././components/Header";
+import Footer from ".././components/Footer";
+
+const jetBrainsMono = Manrope({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+  variable: "--font-Manrope",
+});
+
+export const metadata: Metadata = {
+  title: "CareConnect",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={jetBrainsMono.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
