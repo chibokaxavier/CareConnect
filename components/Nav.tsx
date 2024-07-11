08dd41d8417e67d48321f0f419f05ffd0ae19009
manlikexavy <chibokaxavier@gmail.com>
2024-07-11 12:03:07 +0100

    active link completed

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,38 +6,41 @@ import { usePathname } from "next/navigation";
 const Nav = () => {
   const links = [
     {
-        name: "Home",
-        path: "/",
-      },
-      {
-        name: "Find a doctor",
-        path: "/doctors",
-      },
-      {
-        name: "Services",
-        path: "/services",
-      },
-  
-      {
-        name: "Contact",
-        path: "/contact",
-      },
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Find a doctor",
+      path: "/doctors",
+    },
+    {
+      name: "Services",
+      path: "/services",
+    },
+
+    {
+      name: "Contact",
+      path: "/contact",
+    },
   ];
   const pathName = usePathname();
   return (
     <nav className="flex gap-8">
       {links.map((link, index) => {
         return (
-          <Link
-            href={link.path}
+          <div
             key={index}
-            className={`${
-              link.path === pathName && "text-blue-600 border-b-2 border-accent"
-            }  font-medium hover:text-accent transition-all`}
           >
-            {" "}
-            {link.name}{" "}
-          </Link>
+            <Link
+              href={link.path}
+              className={`${
+                link.path === pathName && "text-blue-600 border-b-2 border-blue-600 "
+              }  font-medium hover:text-accent transition-all`}
+            >
+              {" "}
+              {link.name}{" "}
+            </Link>
+          </div>
         );
       })}{" "}
     </nav>
