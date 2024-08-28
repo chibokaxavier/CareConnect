7c36b4caea8a48bc3efaa26be033519fb1bc70fa
manlikexavy <chibokaxavier@gmail.com>
2024-08-28 21:03:49 +0100

    profile component of doctor profile page in progress

--- app/doctors/_components/Profile.tsx
+++ app/doctors/_components/Profile.tsx
@@ -1,11 +1,69 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+  });
+  const handleInputChange = () => {};
   return (
     <div>
       <h2 className="text-gray-800 font-bold text-[24px] leading-9 mb-10 ">
         Profile information
       </h2>
+      <form action="">
+        <div className="mb-5">
+          <p className="form_label">Name*</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Full Name"
+            className="form_input"
+          />
+        </div>{" "}
+        <div className="mb-5">
+          <p className="form_label">Email</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email "
+            className="form_input"
+            readOnly
+            aria-readonly
+            disabled
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form_label">Phone*</p>
+          <input
+            type="number"
+            name="email"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="Phone number "
+            className="form_input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form_label">Bio*</p>
+          <input
+            type="text"
+            name="bio"
+            value={formData.bio}
+            onChange={handleInputChange}
+            placeholder="Bio "
+            className="form_input"
+            maxLength={100}
+          />
+        </div>
+      </form>
     </div>
   );
 };
